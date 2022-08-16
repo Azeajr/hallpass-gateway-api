30fc5cc7702c899f2ac187ddb8e0592520ecdecb
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-16 15:28:44 -0400

    fix timezone issue between server and client

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,6 +7,7 @@ const db = mariadb.createConnection({
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || '3306', 10),
   database: process.env.DB_NAME,
+  timezone: 'utc',
 });
 
 export default db;
