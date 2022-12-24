62fbbe68cfd092c1fc7b5c5710f6ab545885fc16
Antonio Zea Jr <zeajantonio@gmail.com>
2022-12-23 21:38:56 -0500

    Add crud operations for destinations

--- src/controllers/destinationController.ts
+++ src/controllers/destinationController.ts
@@ -2,6 +2,16 @@ import { Request, Response } from 'express';
 import Location from '../model/Location';
 import User from '../model/User';
 
+const postDestination = async (req: Request, res: Response) => {
+  const { destination } = req.body;
+
+  await Location.create({
+    name: destination,
+  });
+
+  res.send('Destination posted successfully');
+};
+
 const getDestinations = async (req: Request, res: Response) => {
   const locations: any[] = await Location.find({}, 'name').exec();
   const users: any[] = await User.find({}, 'username').exec();
@@ -9,4 +19,35 @@ const getDestinations = async (req: Request, res: Response) => {
   res.send(locations.concat(users).map((destination) => destination.name ?? destination.username));
 };
 
-export default { getDestinations };
+const getDestination = async (req: Request, res: Response) => {
+  const { destinationId } = req.params;
+
+  const location = await Location.findById({ _id: destinationId }).exec();
+
+  res.send(location?.name);
+};
+
+const putDestination = async (req: Request, res: Response) => {
+  const { destinationId } = req.params;
+  const { destinationName } = req.body;
+
+  await Location.findOneAndUpdate({ _id: destinationId }, { name: destinationName }).exec();
+
+  res.send('Destination updated successfully');
+};
+
+const deleteDestination = async (req: Request, res: Response) => {
+  const { destinationId } = req.params;
+
+  await Location.findOneAndRemove({ _id: destinationId }).exec();
+
+  res.send('Destination deleted successfully');
+};
+
+export default {
+  postDestination,
+  getDestinations,
+  getDestination,
+  putDestination,
+  deleteDestination,
+};
--- src/routes/api/destinations.ts
+++ src/routes/api/destinations.ts
@@ -3,6 +3,15 @@ import destinationController from '../../controllers/destinationController';
 
 const router = express.Router();
 
-router.route('/').get(destinationController.getDestinations);
+router
+  .route('/')
+  .post(destinationController.postDestination)
+  .get(destinationController.getDestinations);
+
+router
+  .route('/:destinationId')
+  .get(destinationController.getDestination)
+  .put(destinationController.putDestination)
+  .delete(destinationController.deleteDestination);
 
 export default router;
